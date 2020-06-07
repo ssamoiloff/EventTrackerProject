1d6e506eb5a7196df37f9292a660a2a76ac01cc8
Sergei Samoiloff <sergio.samoiloff@gmail.com>
2020-06-06 21:39:59 -0600

    Completed Address Controller

--- RaveTracker/src/main/java/com/skilldistillery/ravetracker/controllers/AddressController.java
+++ RaveTracker/src/main/java/com/skilldistillery/ravetracker/controllers/AddressController.java
@@ -2,11 +2,16 @@ package com.skilldistillery.ravetracker.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -58,6 +63,60 @@ public class AddressController {
 			e.printStackTrace();
 			resp.setStatus(400);
 			return null;
+		}
+	}
+	
+	@PostMapping("addresses")
+	public Address create(@RequestBody Address addr,
+			HttpServletRequest req,
+			HttpServletResponse resp) {
+		
+		try {
+			addr = svc.createAddress(addr);
+			resp.setStatus(201);
+			StringBuffer url = req.getRequestURL();
+			url.append("/").append(addr.getId());
+			resp.setHeader("Location", url.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+			resp.setStatus(400);
+			addr = null;
+		}
+		return addr;
+	}
+	
+	@PutMapping("addresses/{aid}")
+	public Address update(@PathVariable Integer aid,
+			@RequestBody Address addr,
+			HttpServletRequest req,
+			HttpServletResponse resp) {
+		
+		try {
+			addr = svc.updateAddress(aid, addr);
+			if (addr == null) {
+				resp.setStatus(404);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			resp.setStatus(400);
+			addr = null;
+		}
+		return addr;
+	}
+	
+	@DeleteMapping("addresses/{aid}")
+	public void disable(@PathVariable Integer aid,
+			HttpServletResponse resp) {
+		
+		try {
+			if (svc.disableAddress(aid)) {
+				resp.setStatus(204);
+			} else {
+				resp.setStatus(404);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			resp.setStatus(400);
 		}
 	}
 }
